b0c2bbfcec10b5479b91e31bdc704d0c1e98d39d
rkhexed <64305387+rkhexed@users.noreply.github.com>
2025-01-30 22:43:37 -0500

    Added requirements.txt

--- scraper.py
+++ scraper.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome()
+# Setup ChromeDriver properly
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+# Open Instagram
 driver.get("https://www.instagram.com/")
+
+# Delay for a few seconds to allow the page to load
+time.sleep(5)
+
+# Close the driver after use
+driver.quit()
+
 
